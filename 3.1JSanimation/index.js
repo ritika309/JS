c2b1a9b36e7051a23b5d4c6aa629c56946a121d7
ritika309 <ritikasaini123456@gmail.com>
2022-08-14 23:08:00 +0530

    3.1

---
+++ 3.1JSanimation/index.js
@@ -0,0 +1,12 @@
+//type animation
+var typed= new Typed('#mytext',{
+
+    strings:['ANIMATED TEXT HERE','Hi I am RITIKA'],
+    typeSpeed: 100,
+    showCursor:false
+
+}) 
+//Animation on scroll AOS
+AOS.init({
+    duration:800
+})
